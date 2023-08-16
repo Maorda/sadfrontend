2b355db391c1748958abb5c0e31eb0d9dc4892f6
Maorda <ingdantev@gmail.com>
2023-08-16 18:45:25 -0500

    nuevamente arreglando el servidor ip

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const app = express();
 
 app.use(express.static(__dirname + '/dist'));
 app.get('/*', function(req, res) {
+  console.log(`la ip es:${req.socket.remoteAddress}`)
   res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
 
