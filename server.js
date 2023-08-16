44240639cdcdec5647ca6c2e34a5352dc42b5cd8
Maorda <ingdantev@gmail.com>
2023-08-16 18:51:22 -0500

    mostrar ip

--- server.js
+++ server.js
@@ -5,9 +5,12 @@ const app = express();
 
 app.use(express.static(__dirname + '/dist'));
 app.get('/*', function(req, res) {
-  console.log(`la ip es:${req.socket.remoteAddress}`)
+  
   res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
-
+app.get('/ip', function(req, res) {
+  console.log(`la ip es:${req.socket.remoteAddress}`)
+  res.send(`la ip es:${req.socket.remoteAddress}`) 
+});
 app.listen(process.env.PORT || 4200);
 console.log(`el puerto del frontend es:${process.env.PORT}`)
